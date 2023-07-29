9c8b14ece55cb1547bd9407ea720bf246996880a
yescardmen <emmanuel.grossmann@gmail.com>
2023-07-29 09:10:44 +0200

    Add cpp autocomplete, autopairs plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -57,6 +57,10 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
  }
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
   -- add your plugins here...
   if packer_bootstrap then
     require('packer').sync()
